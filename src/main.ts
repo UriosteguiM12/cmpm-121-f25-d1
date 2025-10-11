a632f87299c39b118b723d73069014230cc87831
UriosteguiM12 <66758770+UriosteguiM12@users.noreply.github.com>
2025-10-11 22:10:43 +0000

    Added an incremental counter that displays on screen (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import pizza from "./pizzaEmoji.png";
 import "./style.css";
 
+// Create a counter
+let counter: number = 0;
+
 // Create a button that has a background image (like in cookie clicker)
 document.body.innerHTML = `
   <img src="${pizza}" id="pizzaButton" class="icon button-like" />
+  <p>Counter: <span id="counter">0</span> pizza slices</p>
 `;
 
 const pizzaButton = document.getElementById("pizzaButton") as HTMLImageElement;
+const counterElement = document.getElementById("counter")!;
 
+// Click logic
 pizzaButton.addEventListener("click", () => {
   console.log("Pizza made!");
+  counter += 1;
+  counterElement.innerHTML = counter.toString();
 });
