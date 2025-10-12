4c4d4471ffbc18e9d3380fd0d2e7430e2ba07cba
UriosteguiM12 <66758770+UriosteguiM12@users.noreply.github.com>
2025-10-12 00:43:12 +0000

    added a rat button that lets the player increment the rate of pizzas per second (PPS) by 1 only when they have 10 pizzas (step 5 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,41 @@
 import pizza from "./pizzaEmoji.png";
+import mouse from "./ratEmoji.png";
 import "./style.css";
 
 // Create a counter
 let counter: number = 0;
+let upgradeCounter: number = 0;
 
 // Create a button that has a background image (like in cookie clicker)
 document.body.innerHTML = `
   <img src="${pizza}" id="pizzaButton" class="icon button-like" />
-  <p>Counter: <span id="counter">0</span> pizza slices</p>
+  <img src="${mouse}" id="mouseButton" class = "icon button-like" />
+  <p>Counter: <span id="counter">0</span> pizzas</p>
 `;
 
 const pizzaButton = document.getElementById("pizzaButton") as HTMLImageElement;
+const mouseButton = document.getElementById("mouseButton") as HTMLButtonElement;
 const counterElement = document.getElementById("counter")!;
+
+mouseButton.disabled = true;
 
 // Determine frame rate
 let start = performance.now();
 
 function update(end: number) {
   const seconds = (end - start) / 1000;
-  console.log(seconds);
   start = end;
 
-  counter += seconds;
+  counter += seconds * upgradeCounter;
   counterElement.textContent = counter.toFixed(0);
+
+  if (counter >= 10) {
+    mouseButton.disabled = false;
+    mouseButton.style.opacity = "1";
+  } else {
+    mouseButton.disabled = true;
+    mouseButton.style.opacity = "0.5";
+  }
 
   requestAnimationFrame(update);
 }
@@ -34,4 +47,10 @@ pizzaButton.addEventListener("click", () => {
   console.log("Pizza made!");
   counter += 1;
   counterElement.innerHTML = counter.toString();
+});
+mouseButton.addEventListener("click", () => {
+  if (counter > 10) {
+    upgradeCounter += 1;
+    counter -= 10;
+  }
 });
