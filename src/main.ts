6dfbc439bdcb3abf8916dbbd9514cb374ac9c217
UriosteguiM12 <66758770+UriosteguiM12@users.noreply.github.com>
2025-10-17 22:03:24 +0000

    added a second upgrade, fixed the PPS for the first upgrade

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,28 @@
 import pizza from "./pizzaEmoji.png";
 import mouse from "./ratEmoji.png";
+import running from "./manRunning.png";
 import "./style.css";
 
 // Create a counter
 let counter: number = 0;
-let upgradeCounter: number = 0;
+let mouseCounter: number = 0;
+let runCounter: number = 0;
 
 // Create a button that has a background image (like in cookie clicker)
 document.body.innerHTML = `
   <img src="${pizza}" id="pizzaButton" class="icon button-like" />
   <img src="${mouse}" id="mouseButton" class = "icon button-like" />
+  <img src="${running}" id="runButton" class = "icon button-like" />
   <p>Counter: <span id="counter">0</span> pizzas</p>
 `;
 
 const pizzaButton = document.getElementById("pizzaButton") as HTMLImageElement;
 const mouseButton = document.getElementById("mouseButton") as HTMLButtonElement;
+const runButton = document.getElementById("runButton") as HTMLButtonElement;
 const counterElement = document.getElementById("counter")!;
 
 mouseButton.disabled = true;
+runButton.disabled = true;
 
 // Determine frame rate
 let start = performance.now();
@@ -26,7 +31,10 @@ function update(end: number) {
   const seconds = (end - start) / 1000;
   start = end;
 
-  counter += seconds * upgradeCounter;
+  counter += (seconds / 10) * mouseCounter; // Mouse upgrades provide 0.10 pizza slices / second
+
+  counter += seconds * runCounter * 2; // Run upgrades provide 2 pizza slices / second
+
   counterElement.textContent = counter.toFixed(0);
 
   if (counter >= 10) {
@@ -35,6 +43,14 @@ function update(end: number) {
   } else {
     mouseButton.disabled = true;
     mouseButton.style.opacity = "0.5";
+  }
+
+  if (counter >= 100) {
+    runButton.disabled = false;
+    runButton.style.opacity = "1";
+  } else {
+    runButton.disabled = true;
+    runButton.style.opacity = "0.5";
   }
 
   requestAnimationFrame(update);
@@ -48,9 +64,17 @@ pizzaButton.addEventListener("click", () => {
   counter += 1;
   counterElement.innerHTML = counter.toString();
 });
+
 mouseButton.addEventListener("click", () => {
-  if (counter > 10) {
-    upgradeCounter += 1;
+  if (counter >= 10) {
+    mouseCounter += 1;
     counter -= 10;
+  }
+});
+
+runButton.addEventListener("click", () => {
+  if (counter >= 100) {
+    runCounter += 1;
+    counter -= 100;
   }
 });
